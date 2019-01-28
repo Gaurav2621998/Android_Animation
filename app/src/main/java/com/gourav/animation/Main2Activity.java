2edfa80d6d5c553fca2c2ae3d1dbfd07f5eb4d9d
gaurav <jaingaurav112@gmail.com>
2019-01-28 16:48:29 +0530

    Update_SharedElement_Animation

--- app/src/main/java/com/gourav/animation/Main2Activity.java
+++ app/src/main/java/com/gourav/animation/Main2Activity.java
@@ -4,11 +4,18 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.transition.Slide;
+import android.transition.TransitionManager;
+import android.view.Gravity;
 import android.view.View;
+
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,14 +35,24 @@ public class Main2Activity extends AppCompatActivity {
     public void onClick(View view) {
 
         ImageView imageView=(ImageView)findViewById(R.id.abc);
-        TextView textView=(TextView)findViewById(R.id.data);
-//        Bundle bundle=ActivityOptions.makeSceneTransitionAnimation(this,imageView,imageView.getTransitionName()).toBundle();
-//        startActivity(new Intent(this,Image_detail.class),bundle);
-        Intent intent = new Intent(this, Image_detail.class);
-        Pair<View, String> p1 = Pair.create((View)view.findViewById(R.id.abc), "profile");
-        Pair<View, String> p3 = Pair.create((View)findViewById(R.id.data), "data");
-        ActivityOptionsCompat options = ActivityOptionsCompat.
-                makeSceneTransitionAnimation(this, p1, p3);
-        startActivity(intent, options.toBundle());
+
+        Slide slide = new Slide();
+        slide.setSlideEdge(Gravity.TOP);
+
+
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        TransitionManager.beginDelayedTransition(root, slide);
+        imageView.setVisibility(View.INVISIBLE);
+
+
+//        TextView textView=(TextView)findViewById(R.id.data);
+////        Bundle bundle=ActivityOptions.makeSceneTransitionAnimation(this,imageView,imageView.getTransitionName()).toBundle();
+////        startActivity(new Intent(this,Image_detail.class),bundle);
+//        Intent intent = new Intent(this, Image_detail.class);
+//        Pair<View, String> p1 = Pair.create((View)view.findViewById(R.id.abc), "profile");
+//        Pair<View, String> p3 = Pair.create((View)findViewById(R.id.data), "data");
+//        ActivityOptionsCompat options = ActivityOptionsCompat.
+//                makeSceneTransitionAnimation(this, p1, p3);
+//        startActivity(intent, options.toBundle());
     }
 }
