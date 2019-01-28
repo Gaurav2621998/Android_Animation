1cb6368233385c5b6a5cd6d217f2080f1a49a5df
gaurav <jaingaurav112@gmail.com>
2019-01-28 16:13:16 +0530

    SharedElement_Animation

--- app/src/main/java/com/gourav/animation/Main2Activity.java
+++ app/src/main/java/com/gourav/animation/Main2Activity.java
@@ -1,7 +1,13 @@
 package com.gourav.animation;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void onClick(View view) {
+
+        ImageView imageView=(ImageView)findViewById(R.id.abc);
+        Bundle bundle=ActivityOptions.makeSceneTransitionAnimation(this,imageView,imageView.getTransitionName()).toBundle();
+        startActivity(new Intent(this,Image_detail.class),bundle);
+
     }
 }
